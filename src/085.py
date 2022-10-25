f4226a93d23b31130c93860446e9901c15408dc7
derbuihan <derbuihan@gmail.com>
2022-10-26 02:07:22 +0900

    solve 085

---
+++ src/085.py
@@ -0,0 +1,32 @@
+from itertools import product
+
+
+def main():
+    K = int(input())
+
+    divisors = []
+    i = 1
+    while i * i < K:
+        if K % i == 0:
+            divisors.append(i)
+            divisors.append(K // i)
+        i += 1
+    else:
+        if i * i == K:
+            divisors.append(i)
+    divisors.sort()
+
+    ans = 0
+    n = len(divisors)
+    for i in range(n):
+        a = divisors[i]
+        for j in range(i, n):
+            b = divisors[j]
+            c = K // (a * b)
+            if a <= b <= c and a * b * c == K:
+                ans += 1
+    print(ans)
+
+
+if __name__ == '__main__':
+    main()
