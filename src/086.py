92b9592ed3274cde7292e01f172ba38e0b5f293b
derbuihan <derbuihan@gmail.com>
2022-10-27 00:42:20 +0900

    fix

--- src/086.py
+++ src/086.py
@@ -5,22 +5,24 @@ def main():
     read = sys.stdin.read
     N, Q, *XYZW = map(int, read().split())
     X, Y, Z, W = XYZW[::4], XYZW[1::4], XYZW[2::4], XYZW[3::4]
+    XYZ = [(x - 1, y - 1, z - 1) for x, y, z in zip(X, Y, Z)]
+    mod = 10 ** 9 + 7
+
+    def func(tmp, shift):
+        for (x, y, z), w in zip(XYZ, W):
+            a_x = (tmp >> x) & 1
+            a_y = (tmp >> y) & 1
+            a_z = (tmp >> z) & 1
+            w_s = (w >> shift) & 1
+            if a_x | a_y | a_z != w_s:
+                return False
+        return True
 
     ans = 1
     for shift in range(60):
-        cnt = 0
-        for tmp in range(2 ** N):
-            for x, y, z, w in zip(X, Y, Z, W):
-                x, y, z = x - 1, y - 1, z - 1
-                if ((tmp >> x) & 1) \
-                        | ((tmp >> y) & 1) \
-                        | ((tmp >> z) & 1) \
-                        != ((w >> shift) & 1):
-                    break
-            else:
-                cnt += 1
-        ans *= cnt
-    print(ans % (7 + 10 ** 9))
+        ans *= sum([func(tmp, shift) for tmp in range(2 ** N)])
+        ans %= mod
+    print(ans)
 
 
 if __name__ == '__main__':
