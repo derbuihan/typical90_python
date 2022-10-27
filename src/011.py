d7ce30677d0e5371440bef327a2877b61053022f
derbuihan <derbuihan@gmail.com>
2022-10-27 19:11:13 +0900

    fix

--- src/011.py
+++ src/011.py
@@ -1,36 +1,34 @@
 import sys
-from itertools import product
 
 
 def main():
     read = sys.stdin.read
     N, *DCS = map(int, read().split())
     D, C, S = DCS[::3], DCS[1::3], DCS[2::3]
+    works = sorted(zip(D, C, S))
 
-    rewards = []
-    for bits in product([0, 1], repeat=N):
-        works = [(D[i], C[i], S[i]) for i in range(N) if bits[i] == 1]
-        works.sort()
-
+    ans = 0
+    for bits in range(2 ** N):
         day = 0
         reward = 0
-        for d, c, s in works:
-            day += c
-            if day > d:
-                break
-            reward += s
+        for j in range(N):
+            if (bits >> j) & 1:
+                d, c, s = works[j]
+                day += c
+                if day > d:
+                    break
+                reward += s
         else:
-            rewards.append(reward)
+            ans = max(reward, ans)
 
-    print(max(rewards))
+    print(ans)
 
 
 def main2():
     read = sys.stdin.read
     N, *DCS = map(int, read().split())
     D, C, S = DCS[::3], DCS[1::3], DCS[2::3]
-    works = [(D[i], C[i], S[i]) for i in range(N)]
-    works.sort()
+    works = sorted(zip(D, C, S))
 
     DP = [[0] * 5001 for i in range(N + 1)]
 
